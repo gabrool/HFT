6594fc583cadc69fbf647cd47a1386a837416749
gabrool <gabrielmetne@gmail.com>
2026-04-13 03:40:02 -0400

    Update RL_exec.py

--- RL_exec.py
+++ RL_exec.py
@@ -5158,7 +5158,7 @@ def run_pipeline(
     mm_ppo_config = PPOConfig(
         lr=float(os.environ.get("BYBIT_MM_PPO_LR", "3e-4")),
         update_epochs=int(os.environ.get("BYBIT_MM_PPO_UPDATE_EPOCHS", "6")),
-        batch_size=int(os.environ.get("BYBIT_MM_PPO_BATCH_SIZE", "32768")),
+        batch_size=int(os.environ.get("BYBIT_MM_PPO_BATCH_SIZE", "131072")),
         clip_ratio=float(os.environ.get("BYBIT_MM_PPO_CLIP_RATIO", "0.2")),
         gamma=float(os.environ.get("BYBIT_MM_PPO_GAMMA", "0.99")),
         gae_lambda=float(os.environ.get("BYBIT_MM_PPO_GAE_LAMBDA", "0.95")),
